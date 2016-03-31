47aef37cc48517f9468d62497e49fbc6e54adb20
pulsar123 <pulsar124@gmail.com>
2016-03-31 11:10:27 -0400

    Update Align.sh

--- Align.sh
+++ Align.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+# Part of Macro-scripts package (a complete Open Source workflow for processing macro focus stacking photographs)
+# Written by Sergey Mashchenko
 
 # Aligning focus stack, starting from the middle frame, and running two processes in parallel in the opposite directions.
-# If something goes wrong you might need to kill one or both running align_image_stack processes manually.
+# This ensures good framing for the aligned stack (minimizes frame waste) and accelerates the computations by 50-100%.
+# Creates a set of aligned images with the names OUT0000.tif, OUT0001.tif and so on.
+# If something goes wrong you might need to kill one or both running align_image_stack processes manually using
+# pkill or kill.
 
 echo
 if test $# -lt 2
@@ -33,22 +38,22 @@ for e in "${files[@]:0:$middle+1}"
     revfiles=( "$e" "${revfiles[@]}" )
   done
 
-# First half:  &>out1.log &
+# First half aligning: 
 align_image_stack "$OPT" --use-given-order -m -a P1_ ${revfiles[@]} &>out1.log &
 
 # A 10s delay, to have a phase shift between two parallel processes:
 sleep 10
 
-# Second half:
+# Second half aligning:
 align_image_stack "$OPT" --use-given-order -m -a P2_ ${files[@]:$middle} &>out2.log &
 
 # Waiting for both processes to finish:
 wait
 
-# Renaming the outputs to become a single aligned stack
 # Deleting the first file in the second half as it is identical to the last file in the first half:
 \rm P2_0000.tif
 
+# Renaming the outputs to become a single aligned stack
 for name in P1_*
  do
  N1=`echo $name |cut -b 4-7`
@@ -63,4 +68,6 @@ for name in P2_*
  mv $name OUT${N2}.tif
  done
 
-
+echo
+echo "Success!"
+echo
